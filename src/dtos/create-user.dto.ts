211969eeef3c6fa8b4fb6a41a88428a452108898
minaadel6600 <51906531+minaadel6600@users.noreply.github.com>
2024-04-04 13:51:15 +0300

    Update create-user.dto.ts

--- src/dtos/create-user.dto.ts
+++ src/dtos/create-user.dto.ts
@@ -1,9 +1,15 @@
 
-interface CreateUserDto {
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  password: string;
-}
+export const createUserValidationSchema = Joi.object({
+  firstName: Joi.string().alphanum().min(3).max(30).required(),
 
-export default CreateUserDto;
+  lastName: Joi.string().required(),
+
+  // repeat_password: Joi.any().valid(Joi.ref('password')).required().messages({
+  //   'any.only' : 'Passwords must match'
+  // }),
+
+  email: Joi.string().email({
+    minDomainSegments: 2, // the minimum number of domain segments (e.g. x.y.z has 3 segments)
+    tlds: { allow: ['com', 'net'] }, // allowed domains
+  }).required(),
+});
