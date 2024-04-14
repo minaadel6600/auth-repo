ccacbe2d0e63b06e3ec093e8f89693e2dc1d1de9
minaadel6600 <51906531+minaadel6600@users.noreply.github.com>
2024-04-14 11:39:38 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,14 @@
-import express from 'express';
 import { Application } from 'express';
 import * as bodyParser from 'body-parser';
 import { MainRouter } from './routes/main.routes';
 import helmet from "helmet";
 import ErrorHandling from './middlewares/error-handlling';
-import mongoose from 'mongoose';
-import { DB_NAME, DB_PORT, DB_URL } from './utils/constants';
-import auth from './middlewares/auth.middleware';
 import cookieParser  from 'cookie-parser';
+import connectDB from './utils/db-connect';
 
 const app: Application = express();
 
-mongoose.connect(`${DB_URL}:${DB_PORT}/${DB_NAME}`).then(() => {
-  console.log("db is connected ...");
-});
-
+connectDB();
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
